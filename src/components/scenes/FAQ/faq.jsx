16d6c52d136a5145ce78d83c4ae5bbb9802642e9
salemaziel <mymainemail0501@gmail.com>
2020-05-24 16:28:48 -0500

    added faq page

---
+++ src/components/scenes/FAQ/faq.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const FAQPage = () => (
+    <>
+        <div>
+            <h1>Frequently Asked Questions</h1>
+        </div>
+    </>
+)
+
+export default FAQPage
---
+++ src/pages/faq.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import FAQPage from '../components/scenes/FAQ/faq'
+
+import Layout from '../utils/layout';
+
+
+
+const FAQ = () => (
+  <Layout hideNav>
+      <FAQPage />
+
+
+  </Layout>
+)
+
+export default FAQ
